2426f4691fbc87e85bbb12d4d45e871f9eb99750
Alex Nguyen <alex-nguyen@live.com.au>
2018-05-08 16:54:52 +1000

    1:35 day 4

---
+++ intro/src/components/Topics.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Link, Route } from 'react-router-dom';
+
+export default function Topics({ match }) {
+  return (
+    <div>
+      {/* Match.url for links - keeps absolute values */}
+      <h2>Topics</h2>
+      <ul>
+        <li>
+          <Link to={`${match.url}/rendering`}>Rendering with React</Link>
+        </li>
+        <li>
+          <Link to={`${match.url}/components`}>Components</Link>
+        </li>
+        <li>
+          <Link to={`${match.url}/props-v-state`}>Props vs State</Link>
+        </li>
+      </ul>
+      <hr />
+      {/* Match.path for routes - keeps dynamic IDs for passinto child components */}
+      {/* Can return your own component in render property if we want to customise behaviour inline */}
+      <Route path={`${match.path}/:topicId`} component={Topic} />
+      <Route
+        exact
+        path={match.url}
+        render={() => <h3>Please select a topic</h3>}
+      />
+    </div>
+  );
+}
+
+// match is prop that react router passes to anything it renders
+function Topic({ match }) {
+  return <h3>{match.params.topicId}</h3>;
+}
